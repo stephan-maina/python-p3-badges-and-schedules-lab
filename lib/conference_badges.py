59cfcf42c9b4f69975054c0ec41984da887d37ed
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-02 03:18:12 +0300

    Update conference_badges.py

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    """Generate a badge for a given name."""
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    """Create a list of badges for a list of names."""
+    badges = [badge_maker(name) for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    """Assign rooms to attendees and generate room assignments."""
+    room_assignments = [f"Hello, {name}! You'll be assigned to room {i + 1}!" for i, name in enumerate(names)]
+    return room_assignments
 
-def printer(names):
-    return None
+def print_badges_and_assignments(names):
+    """Print badges and room assignments for all attendees."""
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    room_assignments = assign_rooms(names)
+    for room_assignment in room_assignments:
+        print(room_assignment)
+
+if __name__ == "__main__":
+    # Updated list of attendees:
+    attendees = ["Stephan", "Mark", "Keren", "Bat-Tziyon", "Samuel", "Pascalia"]
+    print_badges_and_assignments(attendees)
